47105faf754fd7c6154e711554a00cb2c55c81bf
Ben <ben@letskedaddle.com>
2018-05-10 14:30:52 -0400

    debugging lambda function

--- packages/lambda/src/functions/api/registerAsBusiness.js
+++ packages/lambda/src/functions/api/registerAsBusiness.js
@@ -1,6 +1,6 @@
 'use strict';
-// import okayResponse from "../../responses/okayResponse";
-// import errorResponse from "../../responses/errorResponse";
+import okayResponse from "../../responses/okayResponse";
+import errorResponse from "../../responses/errorResponse";
 // import storeToIpfs from '../../services/IpfsService';
 // import storeToS3 from "../../services/S3Service";
 // import s3errorResponse from "../../responses/s3errorResponse";
@@ -9,55 +9,47 @@
 const getBusinessName = function(event) {
 
   const jsonBody = JSON.parse(event.body);
-  return jsonBody.businessAddress;
+  return jsonBody.businessName;
 };
 
 const getBusinessDescription = function(event) {
 
   const jsonBody = JSON.parse(event.body);
-  return jsonBody.businessAddress;
+  return jsonBody.businessDescription;
 };
 
 
 
 export default async (event, context, callback) => {
 
-  // const businessName = getBusinessName(event);
-  // const businessDescription = getBusinessDescription(event);
-  //
-  // if (businessName == null || businessName.length < 1) {
-  //   callback(null, errorResponse);
-  //   return;
-  // }
-  //
-  // if (businessDescription == null || businessDescription.length < 1) {
-  //   callback(null, errorResponse);
-  //   return;
-  // }
-  //
-  // const ipfsObject = JSON.parse(event.body);
-  //
-  // let ipfsHash;
-  //
-  // try {
-  //   ipfsHash = await storeToIpfs(ipfsObject);
-  // } catch (error) {
-  //   return callback(null, ipfsErrorResponse(error));
-  // }
-  //
-  // try {
-  //   await storeToS3(ipfsObject, ipfsHash);
-  // } catch (error) {
-  //   return callback(null, s3errorResponse(error));
-  // }
+  const businessName = getBusinessName(event);
+  const businessDescription = getBusinessDescription(event);
 
-  const okayResponse = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `success`
-    })
-  };
+  if (businessName == null || businessName.length < 1) {
+    callback(null, errorResponse);
+    return;
+  }
 
+  if (businessDescription == null || businessDescription.length < 1) {
+    callback(null, errorResponse);
+    return;
+  }
+
+  const ipfsObject = JSON.parse(event.body);
+
+  let ipfsHash;
+
+  try {
+    ipfsHash = await storeToIpfs(ipfsObject);
+  } catch (error) {
+    return callback(null, ipfsErrorResponse(error));
+  }
+
+  try {
+    await storeToS3(ipfsObject, ipfsHash);
+  } catch (error) {
+    return callback(null, s3errorResponse(error));
+  }
 
   callback(null, okayResponse);
 };
