4492ec370aa85ee37f5f19506436c7f0d4dcf8af
Ben <ben@letskedaddle.com>
2018-05-10 16:22:28 -0400

    debugging lambda function

--- packages/lambda/src/services/S3Service.js
+++ packages/lambda/src/services/S3Service.js
@@ -1,7 +1,8 @@
 'use strict';
 import AWS from "aws-sdk";
 
-const s3 = new AWS.S3({apiVersion: '2006-03-01', region: 'us-east-1'});
+// const s3 = new AWS.S3({apiVersion: '2006-03-01', region: 'us-east-1'});
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 let storeToS3 = async (bucketName, ipfsHash, ipfsObject) => {
 
