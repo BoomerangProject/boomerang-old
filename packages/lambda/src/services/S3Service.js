1b9c0582117a0e8a3b8dd117aff06a9099f77ff1
Ben <ben@letskedaddle.com>
2018-05-10 16:01:14 -0400

    debugging lambda function

--- packages/lambda/src/services/S3Service.js
+++ packages/lambda/src/services/S3Service.js
@@ -7,6 +7,11 @@ let storeToS3 = async (bucketName, ipfsHash, ipfsObject) => {
 
   return new Promise(function(resolve, reject) {
 
+    s3.listBuckets(function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+
     const params = {
       ACL: 'private',
       Bucket: bucketName,
