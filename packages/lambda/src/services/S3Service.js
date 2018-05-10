7844350d8cb8f3aaeafb742ccedefe476f25c082
Ben <ben@letskedaddle.com>
2018-05-10 16:17:31 -0400

    debugging lambda function

--- packages/lambda/src/services/S3Service.js
+++ packages/lambda/src/services/S3Service.js
@@ -7,9 +7,12 @@ let storeToS3 = async (bucketName, ipfsHash, ipfsObject) => {
 
   return new Promise(function(resolve, reject) {
 
-    s3.listBuckets(function(err, data) {
-      if (err) console.log(err, err.stack); // an error occurred
-      else     console.log(data);           // successful response
+    s3.listBuckets(function(error, data) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(JSON.stringify(data));
+      }
     });
 
     const params = {
