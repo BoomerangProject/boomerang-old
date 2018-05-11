92aee77cbf6c14026d52f153cfdfa27be4f024f1
Ben <ben@letskedaddle.com>
2018-05-11 11:33:47 -0400

    debugging lambda function

--- packages/lambda/src/services/S3Service.js
+++ packages/lambda/src/services/S3Service.js
@@ -9,6 +9,7 @@ let storeToS3 = async (bucketName, ipfsHash, ipfsObject) => {
   return new Promise(function(resolve, reject) {
 
     const params = {
+      ACL: 'private',
       Bucket: bucketName,
       Key: ipfsHash,
       Body: JSON.stringify(ipfsObject)
