717557121e8c6b5ddabf96876d4efecf717d8a06
Ben <ben@letskedaddle.com>
2018-05-15 10:26:23 -0400

    refactoring lambda functions

--- packages/lambda/src/functions/api/RegisterAsBusinessSigner.js
+++ packages/lambda/src/functions/api/RegisterAsBusinessSigner.js
@@ -1,7 +1,7 @@
 import web3 from "../../services/Web3HttpService";
 import kudosContract from "../../services/KudosContractService";
 import ipfsHashInBytes from '../../util/IpfsHashStringToBytesConverter';
-import kudosContractAddress from '../../../KudosContractAddress';
+import kudosContractAddress from '../../KudosContractAddress';
 
 const privateKeyOfPayer = '0x4725d5a1c46923e72f04831eab9daf1ec657f256f5e4f139d4835b5197fcffc4';
 const accountAddressOfPayer = '0xdcee2f1da7262362a962d456280a928f4f90bb5e';
