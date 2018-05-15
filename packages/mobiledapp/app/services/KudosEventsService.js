00448bae4d1c18f648c80ad4645398187c1d4126
Ben <ben@letskedaddle.com>
2018-05-15 10:42:01 -0400

    debugging lambda functions

--- packages/mobiledapp/app/services/KudosEventsService.js
+++ packages/mobiledapp/app/services/KudosEventsService.js
@@ -1,5 +1,5 @@
 import web3 from './Web3WebsocketsService.js';
-import kudosContractAddress from '../../../KudosContractAddress';
+import kudosContractAddress from '../KudosContractAddress';
 
 const KudosContractJson = require("kudos-contract-objects/Kudos");
 const kudosContract = new web3.eth.Contract(KudosContractJson.abi, kudosContractAddress);
