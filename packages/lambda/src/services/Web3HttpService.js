4013f8d31859631fd4fc114f2c62175a4c386764
Ben <ben@letskedaddle.com>
2018-06-05 12:12:01 -0400

    switching over to rinkeby

--- packages/lambda/src/services/Web3HttpService.js
+++ packages/lambda/src/services/Web3HttpService.js
@@ -1,4 +1,5 @@
 const Web3 = require('web3');
-const httpProvider = new Web3.providers.HttpProvider('https://ropsten.infura.io/2wx4womFMFUEyRBJKbKq');
+// const httpProvider = new Web3.providers.HttpProvider('https://ropsten.infura.io/2wx4womFMFUEyRBJKbKq');
+const httpProvider = new Web3.providers.HttpProvider('https://rinkeby.infura.io/2wx4womFMFUEyRBJKbKq');
 const web3 = new Web3(httpProvider);
 export default web3;
