2063bb7d75184fb9e68cac03783ac7e2de4f4746
Ben <ben@letskedaddle.com>
2018-07-17 13:14:42 -0400

    debugging lambda function

--- packages/kudos-contracts/test/KudosBusinessTests.js
+++ packages/kudos-contracts/test/KudosBusinessTests.js
@@ -19,24 +19,23 @@ contract("kudosBusinessTests", function([deployer, business, worker, user]) {
   });
 
 
-  it("registerWorkerRewardSystem test", async function() {
-
-    const businessAddress = "0xd0a287acbc9b2b4222c5ea38dc0f8f1031b0e5ce";
-    const numberOfRewardSteps = 3;
-    const numberOfRewardCycles = 4;
-    const numberOfRewardLevels = 5;
-    const levelRewards = [300, 400, 500, 600, 700];
-    const ratingRewards = [0, 25, 50, 75, 100];
-    const ipfsHash = "0x7aec552a65bfd833319cecd80cb10be136a35c9da94a8c899f2536c371293b93";
-
-    await kudos.registerWorkerRewardSystem(businessAddress, numberOfRewardSteps, numberOfRewardCycles, numberOfRewardLevels, levelRewards, ratingRewards, ipfsHash);
-  });
-
-  it("registerWorkerRewardSystem test2", async function() {
-
-    const businessAddress = "0xd0a287acbc9b2b4222c5ea38dc0f8f1031b0e5ce";
-    const workerRewardSystemStruct = await kudos.getWorkerRewardSystem(businessAddress);
-    console.log(workerRewardSystemStruct);
-  });
-
+  // it("registerWorkerRewardSystem test", async function() {
+  //
+  //   const businessAddress = "0xd0a287acbc9b2b4222c5ea38dc0f8f1031b0e5ce";
+  //   const numberOfRewardSteps = 3;
+  //   const numberOfRewardCycles = 4;
+  //   const numberOfRewardLevels = 5;
+  //   const levelRewards = [300, 400, 500, 600, 700];
+  //   const ratingRewards = [0, 25, 50, 75, 100];
+  //   const ipfsHash = "0x7aec552a65bfd833319cecd80cb10be136a35c9da94a8c899f2536c371293b93";
+  //
+  //   await kudos.registerWorkerRewardSystem(businessAddress, numberOfRewardSteps, numberOfRewardCycles, numberOfRewardLevels, levelRewards, ratingRewards, ipfsHash);
+  // });
+  //
+  // it("registerWorkerRewardSystem test2", async function() {
+  //
+  //   const businessAddress = "0xd0a287acbc9b2b4222c5ea38dc0f8f1031b0e5ce";
+  //   const workerRewardSystemStruct = await kudos.getWorkerRewardSystem(businessAddress);
+  //   console.log(workerRewardSystemStruct);
+  // });
 });
